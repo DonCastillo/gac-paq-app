cb4224399af7cd2f6241c09ce8ad87fa2f5e5929
Don <don.qcastillo@yahoo.com>
2023-10-28 19:16:40 -0600

    fixed potential error on the translate button function

--- utils/translate.tsx
+++ utils/translate.tsx
@@ -1,15 +1,20 @@
 import type ButtonInterface from "../interface/button";
 
-function translateButton(buttons: ButtonInterface[], langCode: string | null) {
-	if (!langCode) return null;
-	if (buttons.length === 0) null;
+function translateButton(buttons: ButtonInterface[], langCode: string | null): string | null {
+	if (langCode === null || langCode === undefined || langCode === "") return null;
+	if (buttons.length === 0) return null;
 
 	const translatedButton = buttons.find((button: ButtonInterface) => {
-		const buttonLanguage = button.languages_id.lang_code.toLowerCase();
-		langCode = langCode.toLowerCase();
-		return buttonLanguage === langCode;
+		const buttonLanguage = button?.languages_id?.lang_code?.toLowerCase();
+		const langCodeLowercase = langCode?.toLowerCase();
+		return buttonLanguage === langCodeLowercase;
 	});
-	return translatedButton?.label || null;
+
+	if (translatedButton === undefined || translatedButton === null) {
+		return null;
+	} else {
+		return translatedButton?.label;
+	}
 }
 
 export { translateButton };
