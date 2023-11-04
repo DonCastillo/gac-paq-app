9cfc54ee2ec61b4ccadb62a6cd76876e5f999002
Don <don.qcastillo@yahoo.com>
2023-11-04 04:17:28 -0600

    added doodle graphics to the images

--- styles/images.ts
+++ styles/images.ts
@@ -14,9 +14,22 @@ import EmptyBasket from "./../assets/images/kids/empty_basket.svg";
 import RabbitFaceSVG from "./../assets/images/kids/logo.svg";
 import YellowStrokeLG from "./../assets/images/kids/doodles/stroke_yellow_lg.svg";
 import YellowStroke from "./../assets/images/kids/doodles/stroke_yellow.svg";
+import GreenStrokeLG from "./../assets/images/kids/doodles/stroke_green_lg.svg";
+import GreenStroke from "./../assets/images/kids/doodles/stroke_green.svg";
+import PinkStrokeLG from "./../assets/images/kids/doodles/stroke_pink_lg.svg";
+import PinkStroke from "./../assets/images/kids/doodles/stroke_pink.svg";
+import OrangeStrokeLG from "./../assets/images/kids/doodles/stroke_orange_lg.svg";
+import OrangeStroke from "./../assets/images/kids/doodles/stroke_orange.svg";
+import RedStarsLG from "./../assets/images/kids/doodles/stars_red_lg.svg";
+import RedStars from "./../assets/images/kids/doodles/stars_red.svg";
 import YellowStarsLG from "./../assets/images/kids/doodles/stars_yellow_lg.svg";
 import YellowStars from "./../assets/images/kids/doodles/stars_yellow.svg";
-
+import PurpleStarsLG from "./../assets/images/kids/doodles/stars_purple_lg.svg";
+import PurpleStars from "./../assets/images/kids/doodles/stars_purple.svg";
+import BlueStarsLG from "./../assets/images/kids/doodles/stars_blue_lg.svg";
+import BlueStars from "./../assets/images/kids/doodles/stars_blue.svg";
+import PinkLoopLG from "./../assets/images/kids/doodles/loop_pink_lg.svg";
+import PinkLoop from "./../assets/images/kids/doodles/loop_pink.svg";
 
 const GirlExercising = {
 	uri: require("./../assets/images/adult/girl-exercising.jpg"),
@@ -44,8 +57,22 @@ const Images = {
 		splash_image: RabbitFaceSVG,
 		yellow_stroke_lg: YellowStrokeLG,
 		yellow_stroke: YellowStroke,
+		pink_stroke_lg: PinkStrokeLG,
+		pink_stroke: PinkStroke,
+		green_stroke_lg: GreenStrokeLG,
+		green_stroke: GreenStroke,
+		orange_stroke_lg: OrangeStrokeLG,
+		orange_stroke: OrangeStroke,
 		yellow_stars_lg: YellowStarsLG,
 		yellow_stars: YellowStars,
+		red_stars_lg: RedStarsLG,
+		red_stars: RedStars,
+		purple_stars_lg: PurpleStarsLG,
+		purple_stars: PurpleStars,
+		blue_stars_lg: BlueStarsLG,
+		blue_stars: BlueStars,
+		pink_loop_lg: PinkLoopLG,
+		pink_loop: PinkLoop,
 	},
 	adult: {
 		mostly_not_moving: GirlHeadphones,
