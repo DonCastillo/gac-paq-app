c5051cbd580599b784466dd8094131430db27055
Don <don.qcastillo@yahoo.com>
2023-11-12 12:10:58 -0700

    added success page definition

---
+++ store/data/state-pages/success.ts
@@ -0,0 +1,43 @@
+import ScreenType from "../../../constants/screen_type";
+import type PagePayloadInterface from "../../../interface/directus/page-payload";
+
+const SuccessPage: PagePayloadInterface = {
+	title: "Success Page",
+	name: "Success Page",
+	type: ScreenType.Page,
+	translations: [
+		{
+			id: 1,
+			pages_id: 1,
+			heading: "Success!",
+			description:
+				"Good Job, You finished the Questionnaire.\n\nYour answers have been sent successfully.\n\nThank you for your help!",
+			languages_id: {
+				name: "English - Canada",
+				lang_code: "en-CA",
+			},
+		},
+		{
+			id: 2,
+			pages_id: 1,
+			heading: "Éxito!",
+			description: "Buen trabajo. Has terminado el cuestionario.\n\nTus respuestas se han enviado correctamente.\n\nGracias por tu ayuda.",
+			languages_id: {
+				name: "Spanish - Spain",
+				lang_code: "es-ES",
+			},
+		},
+		{
+			id: 3,
+			pages_id: 1,
+			heading: "Succès!",
+			description: "Bon travail. Vous avez terminé le questionnaire.\n\nVos réponses ont été envoyées avec succès.\n\nMerci pour votre aide.",
+			languages_id: {
+				name: "French - Canada",
+				lang_code: "fr-CA",
+			},
+		},
+	],
+};
+
+export default SuccessPage;
