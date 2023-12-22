a20618bdae8acff2bf4ac9ee1e97b85087b7c8c5
Don <don.qcastillo@yahoo.com>
2023-12-21 22:54:45 -0700

    added "Don't Know" button to the slider component in the kids app

--- components/kid/QuestionSlider.tsx
+++ components/kid/QuestionSlider.tsx
@@ -3,7 +3,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { GeneralStyle } from "../../styles/general";
 import { SettingContext } from "../../store/settings";
 import { Slider } from "@rneui/themed";
-import type PhraseLabel from "../../constants/phrase_label";
+import PhraseLabel from "../../constants/phrase_label";
 
 interface QuestionSliderPropsInterface {
 	onChange: (value: number | PhraseLabel.DontKnow | null) => void;
@@ -17,8 +17,9 @@ export default function QuestionSlider({
 	console.log("slider ...");
 	const settingCtx = useContext(SettingContext);
 	const { colorTheme, currentPage, phrases } = settingCtx.settingState;
-	const { color100 } = colorTheme;
+	const { color100, color200 } = colorTheme;
 	const [value, setValue] = useState<number | PhraseLabel.DontKnow>(selectedValue ?? 0);
+	const MAX_VALUE = 20;
 
 	useEffect(() => {
 		if (value !== selectedValue) {
@@ -26,37 +27,66 @@ export default function QuestionSlider({
 		}
 	}, [currentPage, selectedValue]);
 
-	function changeHandler(value: number): void {
+	function changeHandler(value: number | PhraseLabel.DontKnow): void {
 		setValue(value);
 		onChange(value);
 	}
+
+	function isNumber(value: number | PhraseLabel.DontKnow): boolean {
+		if (value === PhraseLabel.DontKnow) return false;
+		return typeof value === "number" && Number.isInteger(value);
+	}
+
+	function isColor100(value: number | PhraseLabel.DontKnow): string {
+		return isNumber(value) ? color100 : "#fff";
+	}
+
+	function isColor200(value: number | PhraseLabel.DontKnow): string {
+		return isNumber(value) ? color200 : "#fff";
+	}
+
+	function setSliderValue(value: number | PhraseLabel.DontKnow): number | undefined {
+		if (value === PhraseLabel.DontKnow) return undefined;
+		if (isNumber(value) && value >= 0) return value;
+		return undefined;
+	}
+
+	const optionPressedStyle = {
+		backgroundColor: color100,
+		borderColor: color100,
+	};
 
 	return (
 		<View style={styles.container}>
 			{/** slider */}
 			<Slider
 				minimumValue={0}
-				maximumValue={20}
+				maximumValue={MAX_VALUE}
 				step={1}
-				value={value}
+				value={setSliderValue(value)}
 				onValueChange={changeHandler}
-				minimumTrackTintColor={color100}
-				maximumTrackTintColor={color100}
+				minimumTrackTintColor={isColor200(value)}
+				maximumTrackTintColor={isColor100(value)}
 				trackStyle={styles.trackStyle}
-				thumbStyle={{
-					backgroundColor: color100,
-				}}
+				thumbStyle={[
+					styles.thumbStyle,
+					{
+						backgroundColor: isColor100(value),
+					},
+				]}
 				thumbProps={{
 					children: (
 						<View
 							style={[
 								styles.tooltip,
 								{
-									backgroundColor: color100,
+									backgroundColor: isColor100(value),
 								},
 							]}
 						>
-							<Text style={styles.tooltipText}>{value}</Text>
+							<Text style={[styles.tooltipText, { color: isNumber(value) ? "#fff" : "#000" }]}>
+								{isNumber(value) ? value : 0}
+							</Text>
 						</View>
 					),
 				}}
@@ -67,16 +97,16 @@ export default function QuestionSlider({
 				<Pressable
 					style={[
 						styles.optionContainer,
-						// selected === item.value ? optionPressedStyle : styles.optionUnpressed,
+						value === PhraseLabel.DontKnow ? optionPressedStyle : styles.optionUnpressed,
 					]}
-					// onPress={() => {
-					// 	selectHandler(item.value);
-					// }}
+					onPress={() => {
+						changeHandler(PhraseLabel.DontKnow);
+					}}
 				>
 					<Text
 						style={[
 							styles.optionText,
-							// selected === item.value ? styles.textPressed : styles.textUnpressed,
+							value === PhraseLabel.DontKnow ? styles.textPressed : styles.textUnpressed,
 						]}
 					>
 						{phrases?.dontKnow}
@@ -90,23 +120,26 @@ export default function QuestionSlider({
 const styles = StyleSheet.create({
 	container: {
 		marginTop: GeneralStyle.kid.field.marginTop + 20,
-		backgroundColor: "orange",
+		// backgroundColor: "orange",
 	},
 	tooltip: {
 		bottom: 50,
 		justifyContent: "center",
 		alignItems: "center",
 		borderRadius: GeneralStyle.kid.field.borderRadius,
+		borderWidth: GeneralStyle.kid.field.borderWidth,
 		padding: 3,
 	},
 	tooltipText: {
-		fontSize: GeneralStyle.kid.field.fontSize + 10,
-		color: "#fff",
+		fontSize: GeneralStyle.kid.field.fontSize + 5,
 	},
 	trackStyle: {
 		height: 10,
 		borderRadius: GeneralStyle.kid.field.borderRadius,
 		borderColor: "#000",
+		borderWidth: GeneralStyle.kid.field.borderWidth,
+	},
+	thumbStyle: {
 		borderWidth: GeneralStyle.kid.field.borderWidth,
 	},
 	optionContainer: {
