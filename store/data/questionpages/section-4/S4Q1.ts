b482ebd0e55fc1c522e193fba9288fc4d2a3494f
Don <don.qcastillo@yahoo.com>
2024-02-11 14:02:37 -0700

    change mode of transpo question to question radio image payload format

--- store/data/questionpages/section-4/S4Q1.ts
+++ store/data/questionpages/section-4/S4Q1.ts
@@ -1,13 +1,13 @@
 import ScreenType from "constants/screen_type";
-import type QuestionRadioPayloadInterface from "interface/directus/question-radio-payload";
+import type QuestionRadioImagePayloadInterface from "interface/directus/question-radio-image-payload";
 
-const S4Q1: QuestionRadioPayloadInterface = {
+const S4Q1: QuestionRadioImagePayloadInterface = {
 	name: "Question 15:	How did you go to school in the past week?",
 	type: ScreenType.SingleQuestion,
 	translations: [
 		{
 			id: 10,
-			question_radio_id: 8,
+			question_radio_image_id: 8,
 			heading: "Question 15",
 			label: "How did you go to school in the past week?",
 			kid_label: "How did you go to school in the past week?",
@@ -15,24 +15,49 @@ const S4Q1: QuestionRadioPayloadInterface = {
 				"How did your child go to school in the past week? (Select the mode that your child used the most).",
 			choices: [
 				{
-					text: "Walking",
-					value: "Walking",
+					image_choices_id: {
+						id: 1,
+						sort: null,
+						text: "Walking",
+						value: "Walking",
+						image: "",
+					},
 				},
 				{
-					text: "Wheeling (for example, wheelchair use, bicycle, e-bicycle, skateboard, push scooter, rollerblades) ",
-					value: "Wheeling",
+					image_choices_id: {
+						id: 2,
+						sort: null,
+						text: "Wheeling (for example, wheelchair use, bicycle, e-bicycle, skateboard, push scooter, rollerblades)",
+						value: "Wheeling",
+						image: "",
+					},
 				},
 				{
-					text: "School bus",
-					value: "School bus",
+					image_choices_id: {
+						id: 3,
+						sort: null,
+						text: "School bus",
+						value: "School bus",
+						image: "",
+					},
 				},
 				{
-					text: "Public transit (for example, bus, train, tramway, subway, boat)",
-					value: "Public transit",
+					image_choices_id: {
+						id: 3,
+						sort: null,
+						text: "Public transit (for example, bus, train, tramway, subway, boat)",
+						value: "Public transit",
+						image: "",
+					},
 				},
 				{
-					text: "Car, taxi, truck, or motorcycle",
-					value: "Car, taxi, truck, or motorcycle",
+					image_choices_id: {
+						id: 3,
+						sort: null,
+						text: "Car, taxi, truck, or motorcycle",
+						value: "Car, taxi, truck, or motorcycle",
+						image: "",
+					},
 				},
 			],
 			languages_id: {
--- store/data/questionpages/section-4/S4Q4.ts
+++ store/data/questionpages/section-4/S4Q4.ts
@@ -1,7 +1,7 @@
 import ScreenType from "constants/screen_type";
-import type QuestionRadioPayloadInterface from "interface/directus/question-radio-payload";
+import type QuestionRadioImagePayloadInterface from "interface/directus/question-radio-image-payload";
 
-const S4Q4: QuestionRadioPayloadInterface = {
+const S4Q4: QuestionRadioImagePayloadInterface = {
 	name: "Question 16:	How did you go to work in the past week? (Select the mode that you used the most).",
 	type: ScreenType.SingleQuestion,
 	translations: [
@@ -16,24 +16,49 @@ const S4Q4: QuestionRadioPayloadInterface = {
 				"How did your child go to work in the past week? (Select the mode that your child used the most).",
 			choices: [
 				{
-					text: "Walking",
-					value: "Walking",
+					image_choices_id: {
+						id: 1,
+						sort: null,
+						text: "Walking",
+						value: "Walking",
+						image: "",
+					},
 				},
 				{
-					text: "Wheeling (for example, wheelchair use, bicycle, e-bicycle, skateboard, push scooter, rollerblades) ",
-					value: "Wheeling",
+					image_choices_id: {
+						id: 2,
+						sort: null,
+						text: "Wheeling (for example, wheelchair use, bicycle, e-bicycle, skateboard, push scooter, rollerblades)",
+						value: "Wheeling",
+						image: "",
+					},
 				},
 				{
-					text: "School bus",
-					value: "School bus",
+					image_choices_id: {
+						id: 3,
+						sort: null,
+						text: "School bus",
+						value: "School bus",
+						image: "",
+					},
 				},
 				{
-					text: "Public transit (for example, bus, train, tramway, subway, boat)",
-					value: "Public transit",
+					image_choices_id: {
+						id: 3,
+						sort: null,
+						text: "Public transit (for example, bus, train, tramway, subway, boat)",
+						value: "Public transit",
+						image: "",
+					},
 				},
 				{
-					text: "Car, taxi, truck, or motorcycle",
-					value: "Car, taxi, truck, or motorcycle",
+					image_choices_id: {
+						id: 3,
+						sort: null,
+						text: "Car, taxi, truck, or motorcycle",
+						value: "Car, taxi, truck, or motorcycle",
+						image: "",
+					},
 				},
 			],
 			languages_id: {
