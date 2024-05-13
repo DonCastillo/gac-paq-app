f697a2974886c47cdb5ad2768a29844f0f5a37b9
Don <don.qcastillo@yahoo.com>
2024-05-13 17:49:53 -0600

    changed wheeling image to bike

--- styles/images/kids/options.ts
+++ styles/images/kids/options.ts
@@ -2,6 +2,7 @@ import CarSVG from "assets/images/kids/transportation/car.svg";
 import PublicTransportationSVG from "assets/images/kids/transportation/public_transport.svg";
 import SchoolBusSVG from "assets/images/kids/transportation/school_bus.svg";
 import SkateboardSVG from "assets/images/kids/transportation/skateboard.svg";
+import BikeSVG from "assets/images/kids/transportation/bike.svg";
 import SittingTransportationSVG from "assets/images/kids/graphics/sitting.svg";
 import WalkingSVG from "assets/images/kids/transportation/small_walking.svg";
 import NeutralSVG from "assets/images/kids/transportation/neutral_pose.svg";
@@ -29,7 +30,7 @@ const Options = {
 	},
 	transportation: {
 		walking: WalkingSVG,
-		wheeling: SkateboardSVG,
+		wheeling: BikeSVG,
 		car: CarSVG,
 		school_bus: SchoolBusSVG,
 		public_transit: PublicTransportationSVG,
