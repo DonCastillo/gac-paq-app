219941a93ba7442ebc8e6e6493050d86cd6f7b23
Don <don.qcastillo@yahoo.com>
2024-05-16 02:02:11 -0600

    updated section 6 preamble

--- store/data/questionpages/section-6/S6Preamble.ts
+++ store/data/questionpages/section-6/S6Preamble.ts
@@ -13,9 +13,9 @@ const S6Preamble: PreamblePayloadInterface = {
 			heading: "Play or Free Time",
 			subheading: "Section 6",
 			description: {
-				kid: "This includes all the physical activity that you do during your play or free time. This does not include organized activities or break times in school.",
+				kid: "This includes all the physical activity that you do during your play or free time. This does not include organized activities or break times in school. For the next questions, do not include the activities that you just told us about.",
 				adult:
-					"This includes all the physical activity that your child does during their play or free time. This is the time that your child decides how to use.",
+					"This includes all the physical activity that your child does during their play or free time. This is the time that your child decides how to use. For the next questions, do not include the activities that you just told us about.",
 			},
 			languages_id: {
 				name: "English - Canada",
