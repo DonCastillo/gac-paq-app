73693c92313ff11fd37bb0614724791b73a89747
Don <don.qcastillo@yahoo.com>
2024-09-23 01:31:11 -0600

    blacklist ar-AE for the background image

--- store/data/extroductory-pages/AppExtro.ts
+++ store/data/extroductory-pages/AppExtro.ts
@@ -10,6 +10,10 @@ const AppExtro: LangExtroPayloadInterface = {
 			heading: "Thank you for completing the survey!",
 			subheading: "",
 		},
+		"ar-AE": {
+			heading: "!شكرًا لإكمالك الاستطلاع",
+			subheading: "",
+		},
 		"ne-NP": {
 			heading: "सर्वेको पूरा गर्नु भएकोमा धन्यवाद!",
 			subheading: "",
