a9266d1f1f0c54c8eec9369ab3bdd4234b6e6cb7
Don <don.qcastillo@yahoo.com>
2024-09-24 00:15:44 -0600

    translated state pages to en-AE

--- store/data/state-pages/offline-success.ts
+++ store/data/state-pages/offline-success.ts
@@ -12,6 +12,11 @@ const OfflineSuccessPage: LangPagePayloadInterface = {
 			description:
 				"Your responses will be saved and automatically resubmitted when the connection is restored.\n\nThank you for your help!",
 		},
+		"en-AE": {
+			heading: "You are not connected to the Internet",
+			description:
+				"Your responses will be saved and automatically resubmitted when the connection is restored.\n\nThank you for your help!",
+		},
 		"ar-AE": {
 			heading: "لم تتصل بالإنترنت",
 			description:
