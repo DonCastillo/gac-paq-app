298eced112557af9837660ff7e5fc12fb1fff582
Don <don.qcastillo@yahoo.com>
2024-10-20 15:20:09 -0600

    translated words and phrases to ma-IN

--- store/data/phrase/try-again.ts
+++ store/data/phrase/try-again.ts
@@ -4,6 +4,9 @@ const TryAgainPhrase: LangPhraseInterface = {
 	"en-CA": {
 		label: "Try Again",
 	},
+	"ma-IN": {
+		label: "पुन्हा प्रयत्न करा",
+	},
 	"hi-IN": {
 		label: "फिर से कोशिश करें",
 	},
