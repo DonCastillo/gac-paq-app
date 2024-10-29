fe9fafa3bb50eefc2ee14ea2d0714d71a4aa788c
Don <don.qcastillo@yahoo.com>
2024-10-29 00:40:21 -0600

    translated section 3 pages to en-MW

--- store/data/questionpages/section-3/S3Intro.ts
+++ store/data/questionpages/section-3/S3Intro.ts
@@ -10,6 +10,10 @@ const S3Intro: LangSectionPayloadInterface = {
 			heading: "Work",
 			subheading: "Section 3",
 		},
+		"en-MW": {
+			heading: "Work",
+			subheading: "Section 3",
+		},
 		"ch-MW": {
 			heading: "Ntchito",
 			subheading: "Gawo lachitatu",
