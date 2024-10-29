dd0cf2566691fff0d2a39ee92741e04656ca684f
Don <don.qcastillo@yahoo.com>
2024-10-29 00:44:08 -0600

    translated section 4 pages to en-MW

--- store/data/questionpages/section-4/S4Extro.ts
+++ store/data/questionpages/section-4/S4Extro.ts
@@ -10,6 +10,10 @@ const S4Extro: LangExtroPayloadInterface = {
 			heading: "Section Complete!",
 			subheading: "Only three more to go!",
 		},
+		"en-MW": {
+			heading: "Section Complete!",
+			subheading: "Only three more to go!",
+		},
 		"ch-MW": {
 			heading: "Gawo latha",
 			subheading: "Tasala ndi magawo atatu okha kuti timalize",
--- store/data/questionpages/section-4/S4Intro.ts
+++ store/data/questionpages/section-4/S4Intro.ts
@@ -10,6 +10,10 @@ const S4Intro: LangSectionPayloadInterface = {
 			heading: "Transportation",
 			subheading: "Section 4",
 		},
+		"en-MW": {
+			heading: "Transportation",
+			subheading: "Section 4",
+		},
 		"ch-MW": {
 			heading: "Mayendedwe",
 			subheading: "Gawo 4",
