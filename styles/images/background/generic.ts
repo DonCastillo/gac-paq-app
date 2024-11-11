3ec8d27faff6d68aa8a8f18810513650bf2f6ed5
Don <don.qcastillo@yahoo.com>
2024-11-11 04:28:45 -0700

    removed age image on CA

--- styles/images/background/generic.ts
+++ styles/images/background/generic.ts
@@ -2,13 +2,11 @@ const GenericBackground = {
 	CA: {
 		phone: {
 			language_location: require("assets/images/generic/CA/phone/language_location.webp"),
-			age: require("assets/images/generic/CA/phone/age.webp"),
 			mode: require("assets/images/generic/CA/phone/mode.webp"),
 			participant_id: require("assets/images/generic/CA/phone/participant_id.webp"),
 		},
 		tablet: {
 			language_location: require("assets/images/generic/CA/tablet/language_location.webp"),
-			age: require("assets/images/generic/CA/tablet/age.webp"),
 			mode: require("assets/images/generic/CA/tablet/mode.webp"),
 			participant_id: require("assets/images/generic/CA/tablet/participant_id.webp"),
 		},
