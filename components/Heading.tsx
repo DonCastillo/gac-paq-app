9337d69ee2a504909eb0fa0c568b068025ff3c21
Don <don.qcastillo@yahoo.com>
2024-11-17 00:30:58 -0700

    removed font configs from generic components

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
-import { moderateScale } from "utils/responsive.utils";
-import { useSelector } from "react-redux";
-import { getDevice, getLanguage } from "store/settings/settingsSlice";
+import { adjustHeading, adjustWritingDirection } from "utils/style";
 
 interface PropsInterface {
 	children: React.ReactNode;
@@ -10,27 +8,15 @@ interface PropsInterface {
 }
 
 const Heading = ({ children, customStyle = {} }: PropsInterface): React.ReactElement => {
-	const device = useSelector(getDevice);
-	const language = useSelector(getLanguage);
-
 	return (
 		<View>
 			<Text
 				style={[
 					styles.text,
-					{
-						fontSize: moderateScale(
-							device.isTablet ? 45 : 30,
-							device.orientation === "portrait" ? device.screenWidth : device.screenHeight,
-						),
-						lineHeight: moderateScale(
-							device.isTablet ? 60 : 45,
-							device.orientation === "portrait" ? device.screenWidth : device.screenHeight,
-						),
-					},
+					adjustHeading(),
 					customStyle,
 					{
-						writingDirection: language === "ar-AE" ? "rtl" : "ltr",
+						writingDirection: adjustWritingDirection(),
 					},
 				]}
 			>
