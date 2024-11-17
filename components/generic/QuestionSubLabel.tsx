a215d56320da51c26ed42ba392eef6df6027b154
Don <don.qcastillo@yahoo.com>
2024-11-17 00:38:37 -0700

    removed font configs from generic components

--- components/generic/QuestionSubLabel.tsx
+++ components/generic/QuestionSubLabel.tsx
@@ -1,9 +1,7 @@
 import QuestionLabel from "components/kid/QuestionLabel";
 import React, { memo } from "react";
 import { GeneralStyle } from "styles/general";
-import { moderateScale } from "utils/responsive.utils";
-import { useSelector } from "react-redux";
-import { getDevice, getLanguage } from "store/settings/settingsSlice";
+import { adjustQuestionSublabel } from "utils/style";
 
 interface PropsInterface {
 	children: React.ReactNode;
@@ -16,22 +14,12 @@ const QuestionSubLabel = ({
 	customStyle = {},
 	textStyle = {},
 }: PropsInterface): React.ReactElement => {
-	const device = useSelector(getDevice);
-	const language = useSelector(getLanguage);
-
 	if (children !== null && children !== undefined && children !== "") {
 		return (
 			<QuestionLabel
 				textStyle={{
 					...GeneralStyle.adult.questionSubLabel,
-					fontSize: moderateScale(
-						device.isTablet ? (language === "ar-AE" ? 14 : 12) : language === "ar-AE" ? 14 : 12,
-						device.orientation === "portrait" ? device.screenWidth : device.screenHeight,
-					),
-					lineHeight: moderateScale(
-						device.isTablet ? (language === "ar-AE" ? 18 : 16) : language === "ar-AE" ? 18 : 16,
-						device.orientation === "portrait" ? device.screenWidth : device.screenHeight,
-					),
+					...adjustQuestionSublabel(),
 					...textStyle,
 				}}
 				customStyle={{
