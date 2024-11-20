b340b13a39e14052c849e65a28ff0dcf6edfed81
Don <don.qcastillo@yahoo.com>
2024-11-20 01:56:20 -0700

    translated introductory pages to en-NG

--- store/data/introductory-pages/demographic_adult.ts
+++ store/data/introductory-pages/demographic_adult.ts
@@ -157,6 +157,55 @@ const DemographicAdultPage: LangQuestionDropdownPayloadInterface = {
 				},
 			],
 		},
+		"en-NG": {
+			type: Question.QuestionDropdown,
+			heading: "Welcome!",
+			label: "How old is your child?",
+			kid_label: "How old is your child?",
+			adult_label: "How old is your child?",
+			choices: [
+				{
+					label: "8 years old",
+					value: "8",
+				},
+				{
+					label: "9 years old",
+					value: "9",
+				},
+				{
+					label: "10 years old",
+					value: "10",
+				},
+				{
+					label: "11 years old",
+					value: "11",
+				},
+				{
+					label: "12 years old",
+					value: "12",
+				},
+				{
+					label: "13 years old",
+					value: "13",
+				},
+				{
+					label: "14 years old",
+					value: "14",
+				},
+				{
+					label: "15 years old",
+					value: "15",
+				},
+				{
+					label: "16 years old",
+					value: "16",
+				},
+				{
+					label: "17 years old",
+					value: "17",
+				},
+			],
+		},
 		"ch-MW": {
 			type: Question.QuestionDropdown,
 			heading: "Mwalandilidwa!",
