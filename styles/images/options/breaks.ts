1f469718fd1b86f02cd7ba1ed7bf1de4c2e34c12
Don <don.qcastillo@yahoo.com>
2025-01-22 22:51:01 -0700

    formatted image options

--- styles/images/options/breaks.ts
+++ styles/images/options/breaks.ts
@@ -13,16 +13,16 @@ const BreaksOptions = {
 	},
 	CN: {
 		kid: {
-		   not_moving: require("assets/images/kid/CN/options/breaks/not_moving.webp"),
-		   easy_effort: require("assets/images/kid/CN/options/breaks/easy_effort.webp"),
-		   medium_or_hard_effort: require("assets/images/kid/CN/options/breaks/medium_or_hard_effort.webp"),
+			not_moving: require("assets/images/kid/CN/options/breaks/not_moving.webp"),
+			easy_effort: require("assets/images/kid/CN/options/breaks/easy_effort.webp"),
+			medium_or_hard_effort: require("assets/images/kid/CN/options/breaks/medium_or_hard_effort.webp"),
 		},
 		teen: {
-		   not_moving: require("assets/images/teen/CN/options/breaks/not_moving.webp"),
-		   easy_effort: require("assets/images/teen/CN/options/breaks/easy_effort.webp"),
-		   medium_or_hard_effort: require("assets/images/teen/CN/options/breaks/medium_or_hard_effort.webp"),
+			not_moving: require("assets/images/teen/CN/options/breaks/not_moving.webp"),
+			easy_effort: require("assets/images/teen/CN/options/breaks/easy_effort.webp"),
+			medium_or_hard_effort: require("assets/images/teen/CN/options/breaks/medium_or_hard_effort.webp"),
 		},
-	 },
+	},
 	BR: {
 		kid: {
 			not_moving: require("assets/images/kid/BR/options/breaks/not_moving.webp"),
