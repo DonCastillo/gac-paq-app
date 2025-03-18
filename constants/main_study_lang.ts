700cec9bad52ae4ff3eb2bf5e82e55ffacad7c3b
Don <don.qcastillo@yahoo.com>
2025-03-18 01:32:58 -0600

    updated GAC-PAQ pages file, updated main study const

--- constants/main_study_lang.ts
+++ constants/main_study_lang.ts
@@ -1,3 +1,4 @@
-const MAIN_STUDY_LANG = ["sv-SE", "es-MX", "es-ES", "en-CA", "fr-CA", "zh-CN", "pt-BR"];
+// const MAIN_STUDY_LANG = ["sv-SE", "es-MX", "es-ES", "en-CA", "fr-CA", "zh-CN", "pt-BR"];
+const MAIN_STUDY_LANG = ["sv-SE", "es-MX", "es-ES"];
 
 export default MAIN_STUDY_LANG;
