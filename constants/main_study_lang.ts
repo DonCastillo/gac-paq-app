da0eabafdaf5f530354979b1d4e5cc36fc5eea24
Don <don.qcastillo@yahoo.com>
2025-05-08 00:03:03 -0600

    added pt-BR to the main study live

--- constants/main_study_lang.ts
+++ constants/main_study_lang.ts
@@ -1,20 +1,20 @@
-const MAIN_STUDY_LANG = [
-	"sv-SE",
-	"es-MX",
-	"es-ES",
-	"en-CA",
-	"fr-CA",
-	"zh-CN",
-	"pt-BR",
-	"en-AE",
-	"ar-AE",
-	"th-TH",
-	"en-NG",
-	"es-CO",
-	"ne-NP",
-];
+// const MAIN_STUDY_LANG = [
+// 	"sv-SE",
+// 	"es-MX",
+// 	"es-ES",
+// 	"en-CA",
+// 	"fr-CA",
+// 	"zh-CN",
+// 	"pt-BR",
+// 	"en-AE",
+// 	"ar-AE",
+// 	"th-TH",
+// 	"en-NG",
+// 	"es-CO",
+// 	"ne-NP",
+// ];
 
 // main study live
-// const MAIN_STUDY_LANG = ["sv-SE", "es-MX", "es-ES"];
+const MAIN_STUDY_LANG = ["sv-SE", "es-MX", "es-ES", "pt-BR"];
 
 export default MAIN_STUDY_LANG;
