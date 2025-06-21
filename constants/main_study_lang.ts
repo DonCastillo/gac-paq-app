a126b8505cee5637ee859bc1dd87c31a2f38a901
Don <don.qcastillo@yahoo.com>
2025-06-21 03:35:56 -0600

    deploying the Malawi app

--- constants/main_study_lang.ts
+++ constants/main_study_lang.ts
@@ -1,40 +1,42 @@
-const MAIN_STUDY_LANG = [
-	"sv-SE",
-	"es-MX",
-	"es-ES",
-	"en-CA",
-	"fr-CA",
-	"zh-CN",
-	"pt-BR",
-	"en-AE",
-	"ar-AE",
-	"th-TH",
-	"en-NG",
-	"es-CO",
-	"ne-NP",
-	"mi-NZ",
-	"en-NZ",
-	"ch-MW",
-	"en-MW",
-	"en-IN",
-	"hi-IN"
-];
-
-// main study live
 // const MAIN_STUDY_LANG = [
 // 	"sv-SE",
 // 	"es-MX",
 // 	"es-ES",
-// 	"pt-BR",
 // 	"en-CA",
 // 	"fr-CA",
-// 	"en-NG",
-// 	"ne-NP",
 // 	"zh-CN",
-// 	"es-CO",
+// 	"pt-BR",
 // 	"en-AE",
 // 	"ar-AE",
 // 	"th-TH",
+// 	"en-NG",
+// 	"es-CO",
+// 	"ne-NP",
+// 	"mi-NZ",
+// 	"en-NZ",
+// 	"ch-MW",
+// 	"en-MW",
+// 	"en-IN",
+// 	"hi-IN",
 // ];
+
+// main study live
+const MAIN_STUDY_LANG = [
+	"sv-SE",
+	"es-MX",
+	"es-ES",
+	"pt-BR",
+	"en-CA",
+	"fr-CA",
+	"en-NG",
+	"ne-NP",
+	"zh-CN",
+	"es-CO",
+	"en-AE",
+	"ar-AE",
+	"th-TH",
+	"ch-MW",
+	"en-MW",
+];
 
 export default MAIN_STUDY_LANG;
